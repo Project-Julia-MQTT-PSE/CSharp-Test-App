adb1fa3f1ed25b2252e1473c42c31dc9c3e2caf8
Alex <alex@zender.net>
2017-10-18 20:09:12 +0200

    Init project, and basic classes

---
+++ WpfApp2/WpfApp2/Mqtt/Publisher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace WpfApp2.Mqtt
+{
+    class Publisher
+    {
+        private MqttClient _broker = null;
+        private string _id = Guid.NewGuid().ToString();
+        public Publisher(string IP)
+        {
+            _broker = new MqttClient(IP);
+            _broker.Connect(_id);
+        }
+        public Publisher(string topic, byte[] message)
+        {
+            _broker.Publish(topic, message);
+        }
+    }
+}
---
+++ WpfApp2/WpfApp2/Mqtt/Subscriber.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace WpfApp2.Mqtt
+{
+    class Subscriber : INotifyPropertyChanged
+    {
+        private MqttClient _broker = null;
+        private string _id = Guid.NewGuid().ToString();
+        private List<string> _topics = new List<string>();
+        private List<Message> _messages = new List<Message>();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Subscriber(string IP)
+        {
+            _broker = new MqttClient(IP);
+            _broker.Connect(_id);
+            _broker.MqttMsgPublishReceived += (s,e) => 
+            {
+                Message received = new Message();
+                received.Topic = e.Topic;
+                received.Content = e.Message.ToString();
+                Messages.Add(received);
+            };
+        }
+        public void Subscribe(string topic)
+        {
+            Topics.Add(topic);
+            _broker.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        }
+        public List<string> Topics { get => _topics; private set { _topics = value; NotifyPropertyChanged(); } }
+        public List<Message> Messages { get => _messages; private set { _messages = value; NotifyPropertyChanged(); } }
+        //Notify Property Event
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
